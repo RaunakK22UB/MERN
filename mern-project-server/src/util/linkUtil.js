9e0ede3895d4fdaddf0493b2753b846b5b63a2d4
Raunak Singh <120004875+RaunakK22UB@users.noreply.github.com>
2025-07-18 21:43:10 +0530

    browser prblm solved

--- mern-project-server/src/util/linkUtil.js
+++ mern-project-server/src/util/linkUtil.js
@@ -3,13 +3,13 @@
 //
 
 // this the function to get the device information using pre built regx
-const getDeviceInfo=(userAgent)=>{
-    isMobile=/mobile/i.test/{userAgent};
-    const browser=userAgent.match(/(Chrome|Firefox|safari|Edge|Opera)/i)?.[0]||'unknown';
-    return(
+const getDeviceInfo = (userAgent) => {
+    const isMobile = /mobile/i.test(userAgent);
+    const browser = userAgent.match(/(Chrome|Firefox|Safari|Edge|Opera)/i)?.[0] || 'unknown';
+    return {
         isMobile,
         browser
-    );
+    };
 };
 
-module.exports={getDeviceInfo}
+module.exports = { getDeviceInfo };
